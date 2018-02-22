11b897e4c400d41f3185d2cd9bc9e0ca8e5bb2e9
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-02-22 16:13:42 +0000

    layout start

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   get 'charts/drivers_points'
   get 'charts/drivers_rank'
   get '/about', to: 'static_pages#about'
+  get '/home', to: 'static_pages#home' 
   
   resources :constructorresults
   resources :constructors
