a219b010ea53d1afad1a1144f9bd6c77962e6abe
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-02-26 18:12:13 +0000

    routes changes

--- config/routes.rb
+++ config/routes.rb
@@ -2,11 +2,12 @@ Rails.application.routes.draw do
   root 'static_pages#home'
   get 'charts/drivers_points'
   get 'charts/drivers_rank'
-  get 'charts/wins_form'
-  get 'charts/wins_pie_chart'
+  
 
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home' 
+  get '/charts', to: 'charts#wins_form'
+  get '/wins_pie_chart', to: 'charts#wins_pie_chart'
   
   resources :races
   resources :results
