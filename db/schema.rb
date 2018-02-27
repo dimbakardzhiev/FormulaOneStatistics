a9913833ecb0e5cbb7ca8aec104424a57308cfca
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-02-27 09:40:30 +0000

    small routes changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215135302) do
+ActiveRecord::Schema.define(version: 20180226232747) do
 
   create_table "drivers", force: :cascade do |t|
     t.string "forename"
@@ -36,6 +36,11 @@ ActiveRecord::Schema.define(version: 20180215135302) do
     t.integer "grid"
     t.integer "position"
     t.integer "points"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
