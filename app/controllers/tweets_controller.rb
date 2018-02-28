c22356fe9183bab2b29893fa628a4e7be21083c3
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-02-28 16:43:45 +0000

    API complete

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
   	
   def index
 		  @tweets = []
-      $twitter.search('formula one test', result_type: "recent").take(15).collect.each do |object|
+      $twitter.search('formula one', result_type: "recent").take(15).collect.each do |object|
  		  
         if object.is_a?(Twitter::Tweet)
            @tweets.push(object.text)
