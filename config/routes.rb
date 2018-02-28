c45f9eb12aaba8f0f483c69243386e5e95d61fee
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-02-28 21:42:28 +0000

    404 page added

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,26 @@
 Rails.application.routes.draw do
   
-  
-
   root 'static_pages#home'
-  get 'charts/wins_form'
   get 'charts/drivers_rank'
   get 'charts/column_chart_form'
+  
   post 'charts/drivers_points'
   post 'charts/wins_pie_chart'
+  
   get 'api/drivers'
   get 'api/results'
-  get 'api/races'  
+  get 'api/races'
+
   get '/about', to: 'static_pages#about'
   get '/home', to: 'static_pages#home' 
   get '/twitterfeed', to: 'tweets#index'
+  get '/piecharts', to: 'charts#wins_form'
 
   resources :races
   resources :results
   resources :drivers
 
+  match '*a', :to => 'error#not_found', via: :get
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180226232747) do
+ActiveRecord::Schema.define(version: 20180215135302) do
 
   create_table "drivers", force: :cascade do |t|
     t.string "forename"
@@ -36,11 +36,6 @@ ActiveRecord::Schema.define(version: 20180226232747) do
     t.integer "grid"
     t.integer "position"
     t.integer "points"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "tweets", force: :cascade do |t|
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
