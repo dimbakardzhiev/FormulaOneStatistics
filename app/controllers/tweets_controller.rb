5db80377cef85b972af51dd8e5b5267728543051
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-03-02 17:31:22 +0000

    tweets updates again

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,7 +9,7 @@ class TweetsController < ApplicationController
   	
   def index
 		  @tweets = []
-      $twitter.search('#f1 -rt', lang: "en", result_type: "recent").take(15).collect.each do |object|
+      $twitter.search('Lewis Hamilton', lang: "en", result_type: "recent").take(15).collect.each do |object|
  		  
         if object.is_a?(Twitter::Tweet)
            @tweets.push(object.full_text)
