5ba1e818279f0bd5ea22e2c73d1a48ccccd8c326
Dimitar Bakardzhiev <dim.bakardzhiev@gmail.com>
2018-03-02 12:42:11 +0000

    navbar changes

--- app/controllers/charts_controller.rb
+++ app/controllers/charts_controller.rb
@@ -12,7 +12,7 @@ class ChartsController < ApplicationController
 
   def drivers_rank
     @same_driver_boolean = 0
-  	@driver_first = params[:driver_first]
+    @driver_first = params[:driver_first]
     @driver_second = params[:driver_second]
     @driver_first_name = Driver.find(@driver_first[:driver_id]).forename + " " +  Driver.find(@driver_first[:driver_id]).surname
     @driver_second_name = Driver.find(@driver_second[:driver_id]).forename + " " +  Driver.find(@driver_second[:driver_id]).surname
@@ -20,7 +20,7 @@ class ChartsController < ApplicationController
     @results = Result.all
     @year_line_chart = params[:year_line_chart]
     if (@year_line_chart == "") 
-     redirect_to("/linecharts")
+      redirect_to("/linecharts")
     end 
     if @driver_first == @driver_second
       @same_driver_boolean = 1
@@ -28,23 +28,20 @@ class ChartsController < ApplicationController
   end
 
   def wins_form
-  	@results = Result.all
-  	@races = Race.all
-  	#@driver_fullname = Driver.find(@driver[:driver_id]).forename + " " + Driver.find(@driver[:driver_id]).surname
-  end	
+    @results = Result.all
+    @races = Race.all
+    #@driver_fullname = Driver.find(@driver[:driver_id]).forename + " " + Driver.find(@driver[:driver_id]).surname
+  end 
 
   def column_chart_form
   end  
 
   def wins_pie_chart
-  	@driver = params[:driver]
-  	$year = params[:year]
-  	@driver_name = Driver.find(@driver[:driver_id]).forename + " " +  Driver.find(@driver[:driver_id]).surname
-    if $year == "" || @driver[:driver_id] == " "
+    @driver = params[:driver]
+    $year = params[:year]
+    @driver_name = Driver.find(@driver[:driver_id]).forename + " " +  Driver.find(@driver[:driver_id]).surname
+    if $year == ""
      redirect_to("/piecharts")
-    end
-    if @driver[:driver_id] == " " && $year == ""
-      redirect_to("/piecharts") 
     end
   end
 end
